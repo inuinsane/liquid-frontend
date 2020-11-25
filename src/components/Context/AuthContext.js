0cebfbbe8c1747ac17dfecd5b9e09a1b1d828566
Abdullah Ibnu Hasan <38773546+inuinsane@users.noreply.github.com>
2020-11-25 23:02:25 +0800

    create login system

---
+++ src/components/Context/AuthContext.js
@@ -0,0 +1,25 @@
+import React, { createContext, useState } from 'react';
+
+export const AuthContext = createContext();
+
+export const AuthProvider = (props) => {
+    const [auth, setAuth] = useState({
+        currentUser: {
+            id: 0,
+            name: null,
+            email: null,
+            username: null,
+        },
+        hasAccount: true,
+        isLoggedIn: false,
+        loginUrl: 'http://localhost:8000/api/login',
+        registerUrl: 'http://localhost:8000/api/register',
+        profileUrl: 'http:/localhost:8000/api/user/detail',
+    });
+
+    return (
+        <AuthContext.Provider value={[auth, setAuth]}>
+            {props.children}
+        </AuthContext.Provider>
+    )
+}
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
-import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Checkbox from "@material-ui/core/Checkbox";
-import Box from "@material-ui/core/Box";
 import Grid from "@material-ui/core/Grid";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import { Link } from "react-router-dom";
+import { AuthContext } from "./Context/AuthContext";
+import {
+  Container,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@material-ui/core";
+import Axios from "axios";
 
+// Styling
 const useStyles = makeStyles((theme) => ({
   paper: {
     margin: theme.spacing(8, 4),
@@ -34,65 +43,181 @@ const useStyles = makeStyles((theme) => ({
 
 const Login = () => {
   const classes = useStyles();
+  const [show, setShow] = useState(false);
+  const [auth, setAuth] = useContext(AuthContext);
+  const [error, setError] = useState("");
+
+  const [login, setLogin] = useState({
+    username: null,
+    password: null,
+    remember: false,
+  });
+
+  const changeHasAccount = () => {
+    setAuth({ ...auth, hasAccount: !auth.hasAccount });
+  };
+  // Login handle
+  const handleLogin = async (e) => {
+    if (login.username && login.password) {
+      e.preventDefault();
+      const data = {
+        username: login.username,
+        password: login.password,
+      };
+      // Fetch data to server
+      // const response = await Axios.post(auth.loginUrl, data);
+      Axios.post(auth.loginUrl, data).then((res) => {
+        const error = res.data.error;
+        if (error) {
+          console.log(error);
+          setError(error);
+          setShow(true);
+        } else {
+          console.log(res.data);
+          setAuth({
+            ...auth,
+            hasAccount: true,
+            isLoggedIn: true,
+            currentUser: {
+              ...auth.currentUser,
+              id: res.data.user.id,
+              name: res.data.user.name,
+              username: res.data.user.username,
+              email: res.data.user.email,
+            },
+          });
+          setError('');
+          console.log("Login Success");
+          // localStorage.setItem("token", token);
+        }
+      });
+    } else {
+      e.preventDefault();
+      setError("Username dan password harus diisi!");
+      setShow(true);
+    }
+  };
+
+  const handleChange = (e) => {
+    switch (e.target.type) {
+      case "text":
+        setLogin({ ...login, username: e.target.value });
+        break;
+      case "password":
+        setLogin({ ...login, password: e.target.value });
+        break;
+      case "checkbox":
+        setLogin({ ...login, remember: !login.remember });
+        break;
+      default:
+    }
+  };
+
+  const toggleShow = () => {
+    setShow(!show);
+  };
 
   return (
-    <div className={classes.paper}>
-      <Avatar className={classes.avatar}>
-        <LockOutlinedIcon />
-      </Avatar>
-      <Typography component="h1" variant="h5">
-        Login
-      </Typography>
-      <form className={classes.form} noValidate>
-        <TextField
-          //   variant="outlined"
-          margin="normal"
-          required
-          fullWidth
-          id="email"
-          label="Email Address"
-          name="email"
-          autoComplete="email"
-          autoFocus
-        />
-        <TextField
-          //   variant="outlined"
-          margin="normal"
-          required
-          fullWidth
-          name="password"
-          label="Password"
-          type="password"
-          id="password"
-          autoComplete="current-password"
-        />
-        <FormControlLabel
-          control={<Checkbox value="remember" color="primary" />}
-          label="Remember me"
-        />
-        <Button
-          type="submit"
-          fullWidth
-          variant="contained"
-          color="primary"
-          className={classes.submit}
+    <Container component="main" maxWidth="xs">
+      <div className={classes.paper}>
+        <Dialog
+          open={show}
+          onClose={toggleShow}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
         >
-          Sign In
-        </Button>
-        <Grid container>
-          <Grid item xs>
-            <Link to="/" variant="body2">
-              Forgot password?
-            </Link>
+          <DialogTitle id="alert-dialog-title">{"Error!"}</DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              {error}
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={toggleShow} color="primary" autoFocus>
+              OK
+            </Button>
+          </DialogActions>
+        </Dialog>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Login
+        </Typography>
+        <form className={classes.form} noValidate onSubmit={handleLogin}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="username"
+            label="Username"
+            name="username"
+            autoComplete="username"
+            autoFocus
+            type="text"
+            onChange={handleChange}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            onChange={handleChange}
+            autoComplete="current-password"
+          />
+
+          <FormControlLabel
+            control={
+              <Checkbox
+                value="remember"
+                color="primary"
+                type="checkbox"
+                onChange={handleChange}
+              />
+            }
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Login
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link to="#" variant="body2" onClick={toggleShow}>
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid container justify="flex-end">
+              <Grid item>
+                <Typography
+                  variant="body2"
+                  color="textSecondary"
+                  align="center"
+                >
+                  {"Belum punya akun? "}
+                  <Link
+                    to="#"
+                    color="inherit"
+                    href="#"
+                    onClick={changeHasAccount}
+                  >
+                    Silakan registrasi disini.
+                  </Link>
+                </Typography>
+              </Grid>
+            </Grid>
           </Grid>
-          <Grid item>
-            <Link to="/register" variant="body2">
-              {"Belum punya akun? Silakan registrasi dahulu"}
-            </Link>
-          </Grid>
-        </Grid>
-      </form>
-    </div>
+        </form>
+      </div>
+    </Container>
   );
 };
 
