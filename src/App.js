9609b13cedb407796bd47787be25cc153e812d1d
Abdullah Ibnu Hasan <38773546+inuinsane@users.noreply.github.com>
2020-11-25 23:01:42 +0800

    delete import logo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Register from './components/Register';
 import Home from './components/Layouts/Home';
+import { AuthProvider } from './components/Context/AuthContext';
+import Dashboard from './components/Layouts/Dashboard';
 
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/login" component={Login} />
-          <Route path="/register" component={Register} />
-        </Switch>
-      </Router>
-    </div>
+    <AuthProvider>
+      <div className="App">
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
+            <Route path="/dashboard" component={Dashboard} />
+          </Switch>
+        </Router>
+      </div>
+    </AuthProvider>
   );
 }
 
