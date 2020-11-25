b415f01a242818cb1aa02b2656bfeae55a860224
Abdullah Ibnu Hasan <38773546+inuinsane@users.noreply.github.com>
2020-11-25 23:36:33 +0800

    check if user has logged in or not ? redirect to dashboard : login-page

--- src/components/Layouts/Home.js
+++ src/components/Layouts/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useContext } from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
@@ -6,6 +6,8 @@ import { makeStyles } from "@material-ui/core/styles";
 import Register from "../Register";
 import Login from "../Login";
 import { Typography, Link, Box } from "@material-ui/core";
+import { AuthContext } from "../Context/AuthContext";
+import { Redirect } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -27,7 +29,7 @@ const Copyright = () => {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {"Copyright © "}
-      <Link color="inherit" href="https://material-ui.com/">
+      <Link color="inherit" to="/">
         Liquid Enjiniring
       </Link>{" "}
       {new Date().getFullYear()}
@@ -38,14 +40,16 @@ const Copyright = () => {
 
 const Home = () => {
   const classes = useStyles();
-  const [login, setLogin] = useState(true);
+  const [auth] = useContext(AuthContext);
 
-  return (
+  return localStorage.getItem("token") ? (
+    <Redirect to="/dashboard" />
+  ) : (
     <Grid container component="main" className={classes.root}>
       <CssBaseline />
       <Grid item xs={false} sm={4} md={7} className={classes.image} />
       <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-        {login ? <Login /> : <Register />}
+        {auth.hasAccount ? <Login /> : <Register />}
         <Box mt={5}>
           <Copyright />
         </Box>
