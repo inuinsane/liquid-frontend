6fb1e8f21131ff068778ac76c5b5b58de4657967
Abdullah Ibnu Hasan <38773546+inuinsane@users.noreply.github.com>
2020-11-29 11:32:55 +0800

    change the base api endpoint

--- src/components/Context/AuthContext.js
+++ src/components/Context/AuthContext.js
@@ -3,6 +3,7 @@ import React, { createContext, useState } from 'react';
 export const AuthContext = createContext();
 
 export const AuthProvider = (props) => {
+    const baseUrl = "http://api.enj-asm.tech/api";
     const [auth, setAuth] = useState({
         currentUser: {
             id: 0,
@@ -12,9 +13,10 @@ export const AuthProvider = (props) => {
         },
         hasAccount: true,
         isLoggedIn: false,
-        loginUrl: 'http://localhost:8000/api/login',
-        registerUrl: 'http://localhost:8000/api/register',
-        profileUrl: 'http://localhost:8000/api/user/detail',
+        loginUrl: `${baseUrl}/login`,
+        registerUrl: `${baseUrl}/register`,
+        profileUrl: `${baseUrl}/user/detail`,
+        logoutUrl: `${baseUrl}/logout`,
     });
 
     return (
