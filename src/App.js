7f2e50a96f8d1037f5df1f2236a93ed2c9b8f2a7
Abdullah Ibnu Hasan <38773546+inuinsane@users.noreply.github.com>
2020-12-03 22:48:15 +0800

    update file location

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './components/Layouts/Home';
 import { AuthProvider } from './components/Context/AuthContext';
-import Dashboard from './components/Layouts/Dashboard';
+import Dashboard from './components/Dashboard/Dashboard';
+import Home from './components/Home/Home';
 
 
 function App() {
